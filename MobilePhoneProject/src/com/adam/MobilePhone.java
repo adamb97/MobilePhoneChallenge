7501810a1a82aeadc148f4e95e2a498d8fb8a98d
Adam B <47373357+adamb97@users.noreply.github.com>
2020-05-05 16:56:33 -0400

    Update MobilePhone.java

--- MobilePhoneProject/src/com/adam/MobilePhone.java
+++ MobilePhoneProject/src/com/adam/MobilePhone.java
@@ -28,8 +28,11 @@ public class MobilePhone {
         if(foundPosition <0){
             System.out.println(oldContact.getName() + " was not found");
             return false;
+        } else if (findContact(newContact.getName()) != -1){
+            System.out.println("Contact with name " + newContact.getName() + " already exists. update unsuccessful");
+            return false;
         }
-
+        
         this.myContacts.set(foundPosition, newContact);
         System.out.println(oldContact.getName() + " was replaced with " + newContact.getName());
         return true;
